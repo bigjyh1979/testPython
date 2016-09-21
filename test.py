52ca388db76ba5419c5f004d5b1b04a5d055b206
birdlin <birdlin@NB-HP850-D2R.newstl.com.tw>
2016-09-21 18:01:01 +0800

    add comments

--- test.py
+++ test.py
@@ -4,32 +4,28 @@
 @author: birdlin
 '''
 import csv
-avg_money = 0
-money = [0] * 10
-i = 1
-with open ('testCSV.txt', 'r', encoding='utf-8') as fin:
+capacity = 0
+Wh = 0
+
+with open ('rest+discharge.txt', 'r', encoding='utf-8') as fin:
     with open ('newtestCSV.txt', 'w', encoding='utf-8') as fout :
         csvreader = csv.reader(fin, delimiter=',')
         csvwriter = csv.writer(fout, delimiter=',')
         header = next(csvreader)
+        header.append('mAh')                        ## V,I,P,R,T,mAH,WH,mAh
+        header.append('Wh')                         ## V,I,P,R,T,mAH,WH,mAh,Wh
         csvwriter.writerow(header)
         for row in csvreader:
-            row[1]= int(row[1].replace(',', ''))/1000
-            row[2]= round(int(row[2].replace(',', ''))/100000000, 3)
-            row[3]= int(row[3].replace(',', ''))
-            row[4]= float(row[4].replace(',', ''))
-            row[5]= float(row[5].replace(',', ''))
-            if i >= 10 :
-                i = 10
-            j = i-1
-            while j > 0 :
-                money[j] = money[j-1]
-                j -= 1
-            money[0] = row[5]
-            avg_money = sum(money)
-            row.append(avg_money)
-            row.append(avg_money/i)
-            i+=1
-            print (row)
+            row[0]= round(float(row[0]), 3)         ## Voltage
+            row[1]= round(float(row[1]), 3)         ## Current
+            row[2]= round(float(row[2]), 3)         ## I,P,R,T,mAH,WH
+            row[3]= round(float(row[3]), 3)         ## Resistance
+            row[4]= round(float(row[4]), 3)         ## Temperature
+            row[5]= round(float(row[5]), 3)         ## mAh - LM
+            row[6]= round(float(row[6]), 3)         ## Wh - LM
+            capacity += row[1] * 1000 * 10/3600     ## mAh
+            Wh += row[1] * row[0] * 10/3600         ## Wh
+            row.append(round(capacity, 3))          ## Extend Row
+            row.append(round(Wh, 3))                ## Extend Row
             csvwriter.writerow(row)
 
